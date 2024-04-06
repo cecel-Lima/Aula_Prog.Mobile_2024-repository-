706f1c071a617a650b0d523277a4209ebd563649
cecel-Lima <marcel.2021104160@unicap.br>
2024-04-06 19:42:18 -0300

    Giving up on slopes; gonna make a without slopes

--- Aula_Prog.Mobile_2024/Assets/Scripts/CamFollowSkiper.cs
+++ Aula_Prog.Mobile_2024/Assets/Scripts/CamFollowSkiper.cs
@@ -6,19 +6,19 @@ public class CamFollowSkiper : MonoBehaviour
 {
     public Transform skipper;
     Vector3 thisCamPos;
-    float thisPosY;
+    float thisPosZ;
     // Start is called before the first frame update
     void Start()
     {
-        thisPosY = transform.position.y;
+        thisPosZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         thisCamPos = skipper.position;
-        thisCamPos.y = thisPosY;
-        thisCamPos.z = -10f;
+        thisCamPos.z = thisPosZ;
+        thisCamPos.y += 2f;
         transform.position = thisCamPos;
     }
 }
